391cf4dbac76e60fa5312bf3e0edc5b6b9d22a03
zia-hussain <syedziashahgillani76@gmail.com>
2023-12-31 12:03:43 +0500

    Started !

---
+++ first-app/src/App.js
@@ -0,0 +1,13 @@
+// App.jsx
+import React from 'react';
+import Zia from './zia.jsx'; 
+
+function App() {
+  return (
+
+<Zia />
+    
+  );
+}
+
+export default App;
---
+++ first-app/src/zia.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Zia() {
+  return (
+    <>
+      <span>
+        <h2>Name:</h2>
+        <h1>Zia hussain</h1>
+      </span>
+      <span>
+        <h2>Age:</h2>
+        <h1>18 years Old</h1>
+      </span>
+    </>
+  );
+}
+
+export default Zia;
